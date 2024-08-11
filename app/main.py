f4f518c78c8cb65c5a65e740022f59d8db12e07d
Igor Maurício <igor.mauricio.eng@gmail.com>
2024-08-11 17:38:31 -0300

    feat: added routes for login, register, logout, alarms, equipments, battery, generator, fronius inversor, quattro inversor, smart solar charge controller,cerbo gx energy management system and power measurer

--- app/main.py
+++ app/main.py
@@ -6,5 +6,53 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return "Login"
+
+@app.route("/register")
+def register():
+    return "Register"
+
+@app.route("/logout")
+def logout():
+    return "Logout"
+
+@app.route("/alarms")
+def alarms():
+    return "Alarms"
+
+@app.route("/equipments")
+def equipments():
+    return "Equipments"
+
+@app.route("/equipments/battery")
+def battery():
+    return "Battery"
+
+@app.route("/equipments/generator")
+def generator():
+    return "Generator"
+
+@app.route("/equipments/inversor/fronius")
+def inversor_fronnius():
+    return "Fronius"
+
+@app.route("/equipments/inversor/quattro")
+def inversor_quattro():
+    return "Quattro"
+
+@app.route("/equipments/charge_controller/smart_solar")
+def charge_controller_smart_solar():
+    return "Charge Controller Smart Solar"
+
+@app.route("/equipments/energy_management_system/cerbo_gx")
+def charge_controller_cerbo_gx():
+    return "Energy Management System Cerbo GX"
+
+@app.route("/equipments/power_measurer")
+def power_measurement():
+    return "Power Measurer"
+
 if __name__ == "__main__":
     app.run(debug=True, port=33000, host='0.0.0.0')

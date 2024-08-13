72317edc27e50754e843b1675e95583b215a6b34
Igor Maurício <igor.mauricio.eng@gmail.com>
2024-08-12 22:04:07 -0300

    feat: Create models Battery, BatteryLog, Alarm, AlarmLog, UserEvent and UserEventLog

--- app/models.py
+++ app/models.py
@@ -15,4 +15,50 @@ class User(db.Model, UserMixin): # type: ignore
 
 
 
+class Battery(db.Model): # type: ignore
+	id = db.Column(db.Integer, primary_key=True)
+	model = db.Column(db.String(100))
+	manufacturer = db.Column(db.String(100))
+	state = db.Column(db.String(100))
+	health_percent = db.Column(db.String(100))
+	mode = db.Column(db.String(100))
+	relay_status = db.Column(db.String(100))
+	capacity = db.Column(db.Float)
+	min_out_voltage = db.Column(db.Float)
+	max_in_voltage = db.Column(db.Float)
+	charged_percent = db.Column(db.Float)
 	
+
+class BatteryLog(db.Model): # type: ignore
+	id = db.Column(db.Integer, primary_key=True)
+	battery_id = db.Column(db.Integer, db.ForeignKey('battery.id'))
+	current = db.Column(db.Float)
+	voltage = db.Column(db.Float)
+	power = db.Column(db.Float)
+	temperature = db.Column(db.Float)
+	timestamp = db.Column(db.DateTime)
+
+
+class Alarm(db.Model): # type: ignore
+	id = db.Column(db.Integer, primary_key=True)
+	confirmable_by_operator = db.Column(db.Boolean, default=True)
+	message = db.Column(db.String(100))
+	description = db.Column(db.Text)
+	level = db.Column(db.Integer)
+
+class AlarmLog(db.Model): # type: ignore
+	id = db.Column(db.Integer, primary_key=True)
+	alarm_id = db.Column(db.Integer, db.ForeignKey('alarms.id'))
+	confirmed = db.Column(db.Boolean, default=False)
+	confirmed_by_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	timestamp = db.Column(db.DateTime)
+
+class UserEvent(db.Model): # type: ignore
+	id = db.Column(db.Integer, primary_key=True)
+	event = db.Column(db.String(100))
+
+class UserEventLog(db.Model): # type: ignore
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+	timestamp = db.Column(db.DateTime)

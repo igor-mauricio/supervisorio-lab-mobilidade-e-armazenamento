15e2d97ec316cb68cb8d43af3552b5ce79cdea71
Mateus <mateus--henrique@hotmail.com>
2024-08-22 19:38:07 -0300

    Modificação: Coloquei os códigos do template da bateria
    aparentemente nao está funcionando, aqui, só no meu pc, mas depois eu vejo

--- app/static/scripts/minecraft_button.js
+++ app/static/scripts/minecraft_button.js
@@ -1,3 +1,22 @@
-function alertaMinecraft(){
-    alert("Rap do minecraft");
+const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("expand");
+});
+
+function updateDateTime() {
+  var now = new Date();
+  var day = String(now.getDate()).padStart(2, '0');
+  var month = String(now.getMonth() + 1).padStart(2, '0'); // Janeiro é 0!
+  var year = now.getFullYear();
+  var hours = String(now.getHours()).padStart(2, '0');
+  var minutes = String(now.getMinutes()).padStart(2, '0');
+  var seconds = String(now.getSeconds()).padStart(2, '0');
+  
+  var datetimeString = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  
+  document.getElementById("datetime").innerText = datetimeString;
 }
+
+setInterval(updateDateTime, 1000); // Atualiza a cada segundo
+
